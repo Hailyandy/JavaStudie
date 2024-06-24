62c5ac961ab9582c63eb7555a37d2fc7efd5692b
Hailyandy <Larissa00001852@gmail.com>
2024-06-25 06:51:28 +0700

    init project

---
+++ src/studies0/HelloWorld.java
@@ -0,0 +1,10 @@
+package studies0;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+	}
+
+}
